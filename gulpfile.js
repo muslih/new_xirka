f3d37e8a6ad6f930d44f8194b5f26b22f0d14a61
Muslih <muslihzarth@gmail.com>
2017-07-16 16:45:37 +0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,99 @@
+require('es6-promise').polyfill()
+
+var gulp            = require('gulp');
+var nunjucksRender  = require('gulp-nunjucks-render');
+var htmlmin         = require('gulp-htmlmin');
+var postcss         = require('gulp-postcss');
+var sass            = require('gulp-sass');
+var csswring        = require('csswring');
+var autoprefixer    = require('autoprefixer');
+var notify          = require('gulp-notify');
+var plumber         = require('gulp-plumber');
+var jshint          = require('gulp-jshint');
+var concat          = require('gulp-concat');
+var rename          = require('gulp-rename');
+var uglify          = require('gulp-uglify');
+var browserSync     = require('browser-sync').create();
+var reload          = browserSync.reload;
+
+var config = {
+  bootstrapDir: './bower_components/bootstrap-sass',
+  bootstrapJsDir: './bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+  jqueryDir: './bower_components/jquery/dist/jquery.js',
+  publicDir: './app'
+};
+
+var plumberErrorHandler = { errorHandler: notify.onError({
+    title: 'Gulp',
+    message: 'Error: <%= error.message %>'
+  })
+};
+
+gulp.task('css', function() {
+  var processors = [
+    csswring,
+    autoprefixer({browsers:['last 2 version']})
+  ];
+  return gulp.src('./src/css/main.scss')
+  .pipe(plumber(plumberErrorHandler))
+  .pipe(sass({
+      includePaths: [config.bootstrapDir + '/assets/stylesheets'],
+  }))
+  .pipe(postcss(processors))
+  .pipe(gulp.dest(config.publicDir + '/css'));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+  .pipe(gulp.dest(config.publicDir + '/fonts'));
+});
+
+
+gulp.task('js',function(){
+  return gulp.src([config.jqueryDir,config.bootstrapJsDir,'./src/js/**/*.js'])
+  .pipe(plumber(plumberErrorHandler))
+  // .pipe(jshint())
+  .pipe( jshint.reporter( 'fail' ))
+  .pipe(concat('concat.js'))
+  .pipe(gulp.dest('./src/concat'))
+  .pipe(rename('main.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest(config.publicDir+'/js'));
+})
+
+gulp.task('ipau', function(){
+  return gulp.src('./src/pages/**/*.+(html|ipau)')
+  // render template
+  .pipe(nunjucksRender({
+    path:['./src/templates']
+  }))
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest('app'))
+});
+
+gulp.task('watch',function(){
+  gulp.watch('./src/css/*.scss',['css']);
+  gulp.watch('./src/js/**/*.js',['js']);
+
+  gulp.watch('./src/pages/**/*.+(html|ipau)',['ipau']);
+  gulp.watch('./src/templates/**/*.+(html|ipau)',['ipau']);
+
+  gulp.watch("app/**/**/**").on('change', reload);
+
+  // gulp.watch(['./src/css/**/*.**','./src/js/**/*.**','./src/pages/**/*.+(html|ipau)','./src/templates/**/*.+(html|ipau)']).on('change', reload);
+})
+
+gulp.task('serve',['css','fonts','js','ipau','watch'],function(){
+  browserSync.init({
+    open: true,
+    port: 8080,
+    server: {
+      baseDir: "./app"
+    }
+  });
+})
+
+gulp.task('default', ['serve']);
+
+
+
