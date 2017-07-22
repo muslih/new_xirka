96f47e914f3b07f565d4c435dc92a1dfcf073295
Muslih <muslihzarth@gmail.com>
2017-07-22 23:24:13 +0700

    add sliding animation for tagline area

--- src/concat/concat.js
+++ src/concat/concat.js
@@ -12630,5 +12630,16 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-console.log("gulp bootstrap starter");
+$(document).ready(function(){
+  $('#tagline .tagline-box').bind('click', function(){
+    // console.log('clicked man!');
+    
+    $('#tagline .col-md-4').not($(this).parent()).removeClass('active');
+    $(this).parent().toggleClass('active');
+    console.log($(this).parent())
+    
+    // $($(this).data('target')).slideDown();
+    $($(this).data('target')).siblings('.tagline-description-content').removeClass('in');
+  })
+})
 console.log('@muslihzarth');
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log("gulp bootstrap starter");
+$(document).ready(function(){
+  $('#tagline .tagline-box').bind('click', function(){
+    // console.log('clicked man!');
+    
+    $('#tagline .col-md-4').not($(this).parent()).removeClass('active');
+    $(this).parent().toggleClass('active');
+    console.log($(this).parent())
+    
+    // $($(this).data('target')).slideDown();
+    $($(this).data('target')).siblings('.tagline-description-content').removeClass('in');
+  })
+})
